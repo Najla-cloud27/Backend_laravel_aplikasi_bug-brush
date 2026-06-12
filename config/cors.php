3f861c8e6242e7ccf606b76d5592273c99c177cd
Najla Haura <najlahaura27@gmail.com>
2026-06-13 01:38:02 +0700

    fix: update CORS config to support Flutter Web origins via patterns

--- config/cors.php
+++ config/cors.php
@@ -5,14 +5,13 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:5173'),
-        'http://localhost:5173',
-        'http://localhost:5500',
-        'http://127.0.0.1:5500',
-        'http://localhost:3000',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '~^http://localhost(:\d+)?$~',
+        '~^http://127\.0\.0\.1(:\d+)?$~',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 86400,
     'supports_credentials' => true,
 ];
