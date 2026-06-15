a452180e4920255255c22fa94e08326932047bb9
Najla Haura <najlahaura27@gmail.com>
2026-06-16 01:31:26 +0700

    fix: tambah route /download-apk biar download APK lewat Laravel, hindari blokir browser

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing_page');
 });
+
+Route::get('/download-apk', function () {
+    $path = public_path('apk/app-release.apk');
+
+    if (file_exists($path)) {
+        return response()->download($path, 'fokusin.apk', [
+            'Content-Type' => 'application/vnd.android.package-archive',
+        ]);
+    }
+
+    return redirect('http://fokusin.najlahaura.my.id/fokusin.apk');
+});
